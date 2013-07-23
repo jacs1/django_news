16fb0623aaaf690f6b15c0d1f20b8f2ddbf1c5f9
Joseph Simoes <josephsimoes@new-host-2.home>
2013-07-22 23:29:01 -0400

    Added css

--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.utils.timezone import utc
 
 from stories.models import Story
@@ -22,7 +22,6 @@ def top_stories(top=180, consider=1000):
 
 def index(request):
 	stories = top_stories(top=30)
-	return render_to_response('stories/index.html', {'stories': stories})
 
-
-	return HttpResponse(response)
+	return render(request, 'stories/index.html', {'stories': stories})
+		
