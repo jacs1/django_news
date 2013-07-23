b20c2310d1c42052d5e7c7172da0b7f9472d087d
Joseph Simoes <josephsimoes@new-host-2.home>
2013-07-22 23:55:14 -0400

    Added Story Submission Form

--- stories/views.py
+++ stories/views.py
@@ -1,9 +1,12 @@
 import datetime
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.utils.timezone import utc
 
 from stories.models import Story
+from stories.forms import StoryForm
+
 
 
 
@@ -24,4 +27,18 @@ def index(request):
 	stories = top_stories(top=30)
 
 	return render(request, 'stories/index.html', {'stories': stories})
-		
+
+def story(request):
+	if request.method == 'POST':
+		form = StoryForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/')
+
+	else:
+			form = StoryForm()
+	return render(request, 'stories/story.html', {'form': form})
+
+
+
+
